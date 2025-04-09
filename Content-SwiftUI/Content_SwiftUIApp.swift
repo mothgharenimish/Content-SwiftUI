c0e3f3dab0535fd7319eec93f2daed4cc761e540
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-09 21:52:44 +0530

    Initial Commit

---
+++ Content-SwiftUI/Content_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Content_SwiftUIApp.swift
+//  Content-SwiftUI
+//
+//  Created by Nimish Mothghare on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Content_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Content-SwiftUITests/Content_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Content_SwiftUITests.swift
+//  Content-SwiftUITests
+//
+//  Created by Nimish Mothghare on 09/04/25.
+//
+
+import Testing
+@testable import Content_SwiftUI
+
+struct Content_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
