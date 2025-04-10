6a8a5c2bd572be4c6d32e3d8cf18cee7ca87417d
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-10 16:56:38 +0530

    Xcode Update Changes

---
+++ Content-SwiftUI/CustomContainer.swift
@@ -0,0 +1,18 @@
+//
+//  CustomContainer.swift
+//  Content-SwiftUI
+//
+//  Created by Nimish Mothghare on 09/04/25.
+//
+
+import SwiftUI
+
+struct CustomContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomContainer()
+}
